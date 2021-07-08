cc09e7cab498792cc5eff46e9ae42fb5341b729e
whoimi <mail@whoimi.com>
2021-07-08 09:53:35 +0800

    feture:add flowable

--- src/main/java/kim/kin/KkMgrApplication.java
+++ src/main/java/kim/kin/KkMgrApplication.java
@@ -1,12 +1,17 @@
 package kim.kin;
 
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 /**
  * @author choky
@@ -20,5 +25,16 @@ public class KkMgrApplication {
     public static void main(String[] args) {
         SpringApplication.run(KkMgrApplication.class, args);
     }
+    @Bean
+    public CommandLineRunner init(final RepositoryService repositoryService,
+                                  final RuntimeService runtimeService,
+                                  final TaskService taskService) {
 
+        return strings -> {
+//				System.out.println("Number of process definitions : " + repositoryService.createProcessDefinitionQuery().count());
+//				System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+//				runtimeService.startProcessInstanceByKey("oneTaskProcess");
+//				System.out.println("Number of tasks after process start: "+taskService.createTaskQuery().count());
+        };
+    }
 }
